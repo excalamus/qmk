8f6d74f9a64e70e6851257f669ce4a322b814fef
Matthew Trzcinski <matt@excalamus.com>
2023-04-02 14:01:48 -0400

    Update config and binaries with repeat and 1up

--- keyboards/noll/ecosteno/config.h
+++ keyboards/noll/ecosteno/config.h
@@ -1,24 +1,24 @@
-/* Copyright 2021 Nathan Olivares 
- * 
- * This program is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, either version 2 of the License, or 
- * (at your option) any later version. 
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU General Public License for more details. 
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+/* Copyright 2021 Nathan Olivares
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED // recommended by QMK
-#define PRODUCT_ID      0x3621 
+#define PRODUCT_ID      0x3621
 #define DEVICE_VER      0x0100
 #define MANUFACTURER    Noll Electronics LLC
 #define PRODUCT         EcoSteno
@@ -48,5 +48,4 @@
 #define FORCE_NKRO
 
 #define STENO_REPEAT
-//#define STENO_1UP
-
+#define STENO_1UP
